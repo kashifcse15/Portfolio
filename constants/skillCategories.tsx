e5541948c8f36e0c67926690d77d3cba2fbf5f08
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-23 00:40:03 +0530

    added skillCategories

---
+++ constants/skillCategories.tsx
@@ -0,0 +1,32 @@
+export const skillCategories = [
+  {
+    id: "languages",
+    label: "LANGUAGES",
+    number: "01",
+  },
+  {
+    id: "frontend",
+    label: "FRONTEND",
+    number: "02",
+  },
+  {
+    id: "backend",
+    label: "BACKEND",
+    number: "03",
+  },
+  {
+    id: "3d-motion",
+    label: "3D / MOTION",
+    number: "04",
+  },
+  {
+    id: "cloud-devops",
+    label: "CLOUD / DEVOPS",
+    number: "05",
+  },
+  {
+    id: "tools",
+    label: "TOOLS",
+    number: "06",
+  },
+];
